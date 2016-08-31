783337a90131a9a2dd35e35d38b0c772a4e9155d
therealplato <therealplato@gmail.com>
2016-08-31 18:38:01 -0400

    simplify tests

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -3,25 +3,26 @@ package logger
 import (
 	"context"
 	"testing"
-	"time"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
 var ctx0 = context.Background()
+var L1 = logrus.WithFields(logrus.Fields{
+	"test": 1,
+})
+var L2 = logrus.WithFields(logrus.Fields{
+	"test": 2,
+})
 
 func TestFromContext(t *testing.T) {
-	t.Run("given a context with no entry it returns a entry", func(t *testing.T) {
-		ctx1, _ := context.WithTimeout(ctx0, 1*time.Second)
-		result := FromContext(ctx1)
+	t.Run("given a context with no entry, it returns a entry", func(t *testing.T) {
+		result := FromContext(ctx0)
 		assert.IsType(t, &logrus.Entry{}, result)
 	})
 
-	t.Run("given a context with a entry it returns that entry", func(t *testing.T) {
-		L1 := logrus.WithFields(logrus.Fields{
-			"test": 1,
-		})
+	t.Run("given a context with a entry, it returns that entry", func(t *testing.T) {
 		ctx1 := context.WithValue(ctx0, Key, L1)
 		ctx2 := FromContext(ctx1)
 		assert.Equal(t, L1, ctx2)
@@ -29,39 +30,33 @@ func TestFromContext(t *testing.T) {
 }
 
 func TestSetEntry(t *testing.T) {
-	L2 := logrus.WithFields(logrus.Fields{
-		"test": 2,
-	})
 
 	t.Run("given a context with no entry, it returns a new context with the given entry", func(t *testing.T) {
-		ctx := SetEntry(ctx0, L2)
-		assert.NotEqual(t, ctx0, ctx)
-		l := ctx.Value(Key)
+		ctx1 := SetEntry(ctx0, L1)
+		assert.NotEqual(t, ctx0, ctx1)
+		l := ctx1.Value(Key)
 		assert.NotNil(t, l)
 		assert.IsType(t, &logrus.Entry{}, l)
-		assert.Equal(t, L2, l.(*logrus.Entry))
+		assert.Equal(t, L1, l.(*logrus.Entry))
 	})
 
 	t.Run("given a context with a entry, and the same entry, it returns the same context with the same entry", func(t *testing.T) {
-		ctx1 := context.WithValue(ctx0, Key, L2)
-		ctx2 := SetEntry(ctx0, L2)
+		ctx1 := context.WithValue(ctx0, Key, L1)
+		ctx2 := SetEntry(ctx0, L1)
 		assert.Equal(t, ctx1, ctx2)
 		l := ctx2.Value(Key)
 		assert.NotNil(t, l)
 		assert.IsType(t, &logrus.Entry{}, l)
-		assert.Equal(t, L2, l.(*logrus.Entry))
+		assert.Equal(t, L1, l.(*logrus.Entry))
 	})
 
 	t.Run("given a context with a entry, and a different entry, it returns a new context with the new entry", func(t *testing.T) {
-		L3 := logrus.WithFields(logrus.Fields{
-			"test": 3,
-		})
-		ctx1 := context.WithValue(ctx0, Key, L2)
-		ctx2 := SetEntry(ctx0, L3)
+		ctx1 := context.WithValue(ctx0, Key, L1)
+		ctx2 := SetEntry(ctx0, L2)
 		assert.NotEqual(t, ctx1, ctx2)
 		l := ctx2.Value(Key)
 		assert.NotNil(t, l)
 		assert.IsType(t, &logrus.Entry{}, l)
-		assert.Equal(t, L3, l.(*logrus.Entry))
+		assert.Equal(t, L2, l.(*logrus.Entry))
 	})
 }
