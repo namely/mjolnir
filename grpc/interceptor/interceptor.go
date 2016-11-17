21655432d2ccaa359aca0cdf138c21aa0395076a
Robert Ross <robert@creativequeries.com>
2016-11-17 10:31:43 -0500

    Add interceptor for grpc middlewares. (#3)
    * Add interceptor for grpc middlewares.
    
    * Separate Logger interceptor.
    
    * Assert the log lines include the correct fields.

---
+++ grpc/interceptor/interceptor.go
@@ -0,0 +1,49 @@
+package interceptor
+
+import (
+	"golang.org/x/net/context"
+
+	"github.com/Sirupsen/logrus"
+	"google.golang.org/grpc"
+)
+
+// Interceptor makes it easy to return unary interceptors for grpc servers
+type Interceptor struct {
+	logger *logrus.Logger
+	chain  []grpc.UnaryServerInterceptor
+}
+
+// New initializes and returns an Interceptor
+func New() *Interceptor {
+	return &Interceptor{}
+}
+
+// Use adds a middleware to the chain for the server interceptor
+func (i *Interceptor) Use(m grpc.UnaryServerInterceptor) {
+	i.chain = append(i.chain, m)
+}
+
+// Middleware returns a grpc.UnaryServerHandler compatible function
+// documented here https://github.com/grpc/grpc-go/tree/master/interceptor.go#L73
+// It will handle intercepting errors and unwrapping the original error for logging.
+//
+// A grpc endpoint should return generic errors such as "company not found" by wrapping
+// the original error.
+// This chaining logic shamelessly stolen from https://github.com/mwitkow/go-grpc-middleware/blob/master/chain.go
+func (i *Interceptor) Middleware() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		buildChain := func(current grpc.UnaryServerInterceptor, next grpc.UnaryHandler) grpc.UnaryHandler {
+			return func(currentCtx context.Context, currentReq interface{}) (interface{}, error) {
+				return current(currentCtx, currentReq, info, next)
+			}
+		}
+
+		chain := handler
+
+		for ii := len(i.chain) - 1; ii >= 0; ii-- {
+			chain = buildChain(i.chain[ii], chain)
+		}
+
+		return chain(ctx, req)
+	}
+}
---
+++ grpc/interceptor/logger_test.go
@@ -0,0 +1,61 @@
+package interceptor
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+
+	"github.com/namely/mjolnir/logger"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// LogLine is only used to parse log lines from interceptors making it
+// easier to test
+type logLine struct {
+	Msg       string `json:"msg"`
+	RequestID string `json:"request_id"`
+	Duration  string `json:"duration"`
+}
+
+func TestLoggerInterceptor(t *testing.T) {
+	i := New()
+	l := logrus.New()
+	l.Formatter = &logrus.JSONFormatter{}
+	buf := new(bytes.Buffer)
+	l.Out = buf
+
+	i.Use(Logger(l))
+	middleware := i.Middleware()
+	ctx := context.TODO()
+	info := &grpc.UnaryServerInfo{FullMethod: "/package.service/method"}
+	final := func(ctx context.Context, in interface{}) (interface{}, error) {
+		e := logger.FromContext(ctx)
+		e.Info("from handler")
+		return nil, nil
+	}
+
+	middleware(ctx, nil, info, final)
+
+	decoder := json.NewDecoder(buf)
+	var firstLine logLine
+	require.NoError(t, decoder.Decode(&firstLine))
+	assert.Equal(t, "processing rpc", firstLine.Msg)
+	assert.NotEmpty(t, firstLine.RequestID)
+
+	var secondLine logLine
+	require.NoError(t, decoder.Decode(&secondLine))
+	assert.Equal(t, "from handler", secondLine.Msg)
+	assert.NotEmpty(t, secondLine.RequestID)
+
+	var thirdLine logLine
+	require.NoError(t, decoder.Decode(&thirdLine))
+	assert.Equal(t, "finished rpc", thirdLine.Msg)
+	assert.NotEmpty(t, thirdLine.RequestID)
+	assert.NotEmpty(t, thirdLine.Duration)
+}
