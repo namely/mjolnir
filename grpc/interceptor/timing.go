6d3a4ce178c02dde8506692f3aec5256ee1e9a80
BobbyTables <robert@creativequeries.com>
2017-03-08 12:53:50 -0500

    Use go-metrics to standardize how the timer middleware work.

--- grpc/interceptor/timing.go
+++ grpc/interceptor/timing.go
@@ -7,26 +7,12 @@ import (
 
 	"golang.org/x/net/context"
 
+	"github.com/armon/go-metrics"
 	"google.golang.org/grpc"
 )
 
 // regex to pull out the rpc name from the FullMethod tag on the call info
 var methodRegex = regexp.MustCompile(`^\/[\w\.]+\/([\w]+)`)
-
-// StatsdClient is an interface to allow passing in any statsd-esque client.
-// Since we rely on the datadog one usually, this is where this is stolen from.
-// See: https://godoc.org/github.com/DataDog/datadog-go/statsd
-type StatsdClient interface {
-	Count(name string, value int64, tags []string, rate float64) error
-	Decr(name string, tags []string, rate float64) error
-	Gauge(name string, value float64, tags []string, rate float64) error
-	Histogram(name string, value float64, tags []string, rate float64) error
-	Incr(name string, tags []string, rate float64) error
-	Set(name string, value string, tags []string, rate float64) error
-	SimpleEvent(title, text string) error
-	TimeInMilliseconds(name string, value float64, tags []string, rate float64) error
-	Timing(name string, value time.Duration, tags []string, rate float64) error
-}
 
 // Timer is a grpc middleware for handling timing how long endpoints take to
 // handle requests and send them to the client given.
@@ -39,17 +25,17 @@ type StatsdClient interface {
 //
 // * INCR production.myapp.CreateThing
 // * TIMING production.myapp.CreateThing.duration
-func Timer(prefix string, c StatsdClient) grpc.UnaryServerInterceptor {
+func Timer(m *metrics.Metrics) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		groups := methodRegex.FindStringSubmatch(info.FullMethod)
 		if groups[1] == "" {
 			return nil, errors.New("interceptor/timing: could not find method in grpc info")
 		}
 
-		key := prefix + "." + groups[1]
-		c.Incr(key, nil, 1)
+		key := []string{groups[1]}
+		m.IncrCounter(key, 1)
 		start := time.Now()
-		defer c.Timing(key+".duration", time.Since(start), nil, 0)
+		defer m.MeasureSince(key, start)
 		return handler(ctx, req)
 	}
 }
