767b3cd1eb68755389f701b22e168794165c3241
Ehud Cohen <ehud.cohen@namely.com>
2017-03-10 16:28:43 -0500

    add ErrorFielder and ErrorKeyer optional interfaces

---
+++ data/errors.go
@@ -0,0 +1,66 @@
+package data
+
+import (
+	"github.com/Sirupsen/logrus"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+)
+
+// ErrorKeyer declares something that fulfills error interface and has a key.
+// We will use this when we want machine readable error string alongside error message (e.g. proto errors)
+// implements error interface in order to be passed around as an error
+type ErrorKeyer interface {
+	error
+	Key() string
+}
+
+// ErrorFielder declares something that fulfills error interface and has Logrus error fields.
+// We will use this to pass error fields from handlers to middlewares for Logs
+// implements error interface in order to be passed around as an error
+type ErrorFielder interface {
+	error
+	Fields() *logrus.Fields
+}
+
+// KeyedErr implements ErrorKeyer
+type KeyedErr struct {
+	error
+	Message  string
+	ErrorKey string
+}
+
+// FieldErr implements ErrorFielder
+type FieldErr struct {
+	error
+	ErrFields *logrus.Fields
+}
+
+// NewFieldErr creates a new error with fields
+func NewFieldErr(fields *logrus.Fields) *FieldErr {
+	return &FieldErr{ErrFields: fields}
+}
+
+// Fields returns the fields for this error
+func (e *FieldErr) Fields() *logrus.Fields {
+	return e.ErrFields
+}
+
+// Error returns the error message
+// Needed to fulfill interface requirement (error)
+func (e *FieldErr) Error() string {
+	return "Don't use me, just use my Fields() method for logs!"
+}
+
+// Error returns the error message
+func (e *KeyedErr) Error() string {
+	return e.Message
+}
+
+// Key returns the error key
+func (e *KeyedErr) Key() string {
+	return e.ErrorKey
+}
+
+// ErrGrpcInternalError indicates an accounts internal server error
+var ErrGrpcInternalError = grpc.Errorf(codes.Unknown, "internal server error")
--- grpc/interceptor/logger.go
+++ grpc/interceptor/logger.go
@@ -5,10 +5,12 @@ import (
 
 	"golang.org/x/net/context"
 
+	"../../data"
 	"github.com/Sirupsen/logrus"
 	"github.com/namely/mjolnir/logger"
 	uuid "github.com/satori/go.uuid"
 	"google.golang.org/grpc"
+	"regexp"
 )
 
 // Logger returns an interceptor that will set on the context a *logrus.Entry
@@ -19,22 +21,33 @@ func Logger(l *logrus.Logger) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		ctx = addLoggerToContext(l, ctx)
 		entry := logger.FromContext(ctx)
-		name := info.FullMethod
+
+		// regex to change /service.Service/Endpt -> Endpt
+		r := regexp.MustCompile(`\/([A-Za-z])\w+\.([A-Za-z])\w+\/`)
+		name := r.ReplaceAllString(info.FullMethod, "")
 
 		entry.WithField("endpoint", name).Info("processing rpc")
 
 		start := time.Now()
 		out, err := handler(ctx, req)
 		if err != nil {
-			entry.WithError(err).WithField(
-				"duration", time.Since(start).String(),
-			).Error("rpc endpoint failed")
-			return nil, err
+			if ferr, ok := err.(data.ErrorFielder); ok {
+				fields := ferr.Fields()
+				entry.WithError(ferr).WithFields(*fields).WithField(
+					"duration", time.Since(start).String(),
+				).Error("rpc endpoint " + name + " failed")
+			} else {
+				entry.WithError(err).WithField(
+					"duration", time.Since(start).String(),
+				).Error("rpc endpoint " + name + " failed")
+			}
+			return nil, data.ErrGrpcInternalError
 		}
 
 		entry.WithFields(logrus.Fields{
-			"endpoint": name,
-			"duration": time.Since(start).String(),
+			"endpoint":    name,
+			"pb_response": out,
+			"duration":    time.Since(start).String(),
 		}).Info("finished rpc")
 
 		return out, err
